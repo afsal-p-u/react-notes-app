b9dc5bda588fd6e4d108ef6ae6338230560bb2ae
Afsal p u <afsalpu16@gmail.com>
2022-09-12 10:37:12 +0530

    Updates

---
+++ precache-manifest.67cf9ece574ef3b942d826f7354bde19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7208c6341ca2607a5558",
+    "url": "/react-notes-app/static/css/main.af8c9fd4.chunk.css"
+  },
+  {
+    "revision": "7208c6341ca2607a5558",
+    "url": "/react-notes-app/static/js/main.7208c634.chunk.js"
+  },
+  {
+    "revision": "481b4b72d2f11530a6ce",
+    "url": "/react-notes-app/static/js/1.481b4b72.chunk.js"
+  },
+  {
+    "revision": "69973f7d145d383cce50",
+    "url": "/react-notes-app/static/js/runtime~main.69973f7d.js"
+  },
+  {
+    "revision": "16a70597fdb240928f7478668a35f65b",
+    "url": "/react-notes-app/index.html"
+  }
+];
